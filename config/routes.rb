17f7a9b5289973b2616bab819270cf6dc70a4d0b
5MatheusFraga7 <fragamatheus823@gmail.com>
2023-10-18 23:17:44 -0300

    Adicionando novas funções de criação de personagens

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  get '/characters', to: 'character_class#show', defaults: { format: 'json' }
+  get '/characters'           , to: 'character_class#show', defaults: { format: 'json' }
+  get '/classes'              , to: 'character_class#classes', defaults: { format: 'json' }
+  post '/characters/create'   , to: 'character_class#create', defaults: { format: 'json' }
+
 end
