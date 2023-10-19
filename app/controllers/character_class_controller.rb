1bfacbb1978039c1c775ad66e36a771353133bcf
5MatheusFraga7 <fragamatheus823@gmail.com>
2023-10-18 23:36:53 -0300

    Finalizando o crud de personagens

--- app/controllers/character_class_controller.rb
+++ app/controllers/character_class_controller.rb
@@ -1,44 +1,11 @@
 class CharacterClassController < ApplicationController
   protect_from_forgery with: :null_session
-  def show
-    respond_to do |format|
-      data = Character.joins("JOIN races on (race_id = races.id)")
-                      .joins("JOIN character_classes on (class_id = character_classes.id)")
-                      .select("races.name as race, characters.name, characters.id, character_classes.name as classe")
-      format.json { render json: { status: 'success', character_class: data }, status: 200 }
-    end
-  end
 
-  def classes
+  def read
     respond_to do |format|
       data = CharacterClass.select("id, name, description")
       format.json { render json: { status: 'success', classes: data }, status: 200 }
     end
   end
-
-  def create 
-    character = Character.new(character_params)
-
-    if (character.save)
-      respond_to do |format|
-        format.json { render json: { status: 'success', character: character_params }, status: 200 }
-      end
-      return 
-
-    else
-      respond_to do |format|
-        format.json { render json: { status: 'error', erors: character.erors }, status: 500 }
-      end
-      return 
-    end
-
-  end
-
-  private
-
-  def character_params
-    params.require(:character).permit(:user_id, :race_id, :class_id, :name, :level, :health_points, :actual_health_points, :inspiration, :charism, :inteligence, :wisdom, :constituition, :dezterity, :strength, :align, :size)	
-  end
-
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
-  get '/characters'           , to: 'character_class#show', defaults: { format: 'json' }
-  get '/classes'              , to: 'character_class#classes', defaults: { format: 'json' }
-  post '/characters/create'   , to: 'character_class#create', defaults: { format: 'json' }
+
+  get '/characters'           , to: 'character#read', defaults: { format: 'json' }
+  post '/characters/create'   , to: 'character#create', defaults: { format: 'json' }
+  put '/characters/update'    , to: 'character#update', defaults: { format: 'json' }
+  delete '/characters/delete' , to: 'character#delete', defaults: { format: 'json' }
+
+  get '/classes'              , to: 'character_class#read', defaults: { format: 'json' }
 
 end
