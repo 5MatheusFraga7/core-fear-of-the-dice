fea1a546fd6b088f8fbb91d37e6e189adbac8af3
5MatheusFraga7 <fragamatheus823@gmail.com>
2023-10-19 00:57:26 -0300

    Configurando cors

--- app/controllers/character_controller.rb
+++ app/controllers/character_controller.rb
@@ -23,7 +23,7 @@ class CharacterController < ApplicationController
     respond_to do |format|
       data = Character.joins("JOIN races on (race_id = races.id)")
                       .joins("JOIN character_classes on (class_id = character_classes.id)")
-                      .select("races.name as race, characters.name, characters.id, character_classes.name as classe")
+                      .select("races.name as race, characters.name, characters.id, character_classes.name as className")
       format.json { render json: { status: 'success', character_class: data }, status: 200 }
     end
   end
